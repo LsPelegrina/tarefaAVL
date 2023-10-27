2fff7da49fdb46c887466ed6b926af434f3dc531
LsPelegrina <141710271+LsPelegrina@users.noreply.github.com>
2023-10-27 12:11:06 -0300

    Update main.c
    Mudança no include para bst.c 
    
    Troca da função para AVLinsert

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-#include "bst.h"
+#include "bst.c" // mudanca
 
-int main () {
+int main () { // mudanca na adicao de nos na arvore
   Tree t = createTree();
   link x;
-  insert(t, 99);
-  imprime(t, "oi");
-  insert(t, 88);
-  imprime(t, "oi");
-  insert(t, 77);
+  AVLinsert(t, 40);
+  AVLinsert(t, 10);
+  AVLinsert(t, 30);
+  AVLinsert(t, 20);
+  AVLinsert(t, 90);
+  AVLinsert(t, 80);
+  AVLinsert(t, 70);
+  AVLinsert(t, 100);
+  AVLinsert(t, 25);
+  AVLinsert(t, 90);
+  AVLinsert(t, 90);
   imprime(t, "oi");
 
   return 0;
